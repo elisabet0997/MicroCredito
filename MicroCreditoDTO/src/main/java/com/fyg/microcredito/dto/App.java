3fac82b7c6641e2acfd0e22fd1fdf8c754684b6c
Elisabet Adyani Vasquez Martinez <elisabet.vasquez@fygsolutions.com>
2016-09-28 10:31:34 -0500

    Adding files via upload

---
+++ MicroCreditoDTO/src/main/java/com/fyg/microcredito/dto/App.java
@@ -0,0 +1,13 @@
+package com.fyg.microcredito.dto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
